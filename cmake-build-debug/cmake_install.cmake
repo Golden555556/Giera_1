750c085b4ce0de4dab65e82544b6c62aa2c92bf4
Golden555556 <krystian5.luczak@gmail.com>
2025-01-30 13:31:43 +0100

    second commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -37,6 +37,11 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "D:/CLion 2024.2.2/bin/mingw/bin/objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/GitHub/Giera_v1/cmake-build-debug/_deps/fmt-build/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
 
+#include "SFML/Graphics.hpp"
+#include "fmt/ranges.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //--------------------------------------------INTIALIZE--------------------------------------------
+    //create the window
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 16;
+    sf::RenderWindow window(sf::VideoMode(800,600), "RPG GAME", sf::Style::Default, settings);
+
+    sf::CircleShape triangle(50.f,3);
+    triangle.setFillColor(sf::Color::Red);
+    triangle.setOutlineThickness(10.f);
+    triangle.setOutlineColor(sf::Color::White);
+    triangle.setPosition(sf::Vector2f(100,100));
+
+    sf::CircleShape octagon(50.f, 8);
+    octagon.setFillColor(sf::Color::Blue);
+    octagon.setOutlineThickness(8.f);
+    octagon.setOutlineColor(sf::Color::Green);
+    octagon.setPosition(sf::Vector2f(100,100));
+
+    //--------------------------------------------INTIALIZE--------------------------------------------
+
+    //--------------------------------------------UPDATE-----------------------------------------------
+
+    //run the  program as long as the window is open
+    while(window.isOpen())
+    {
+        //check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            //"close requested" event: we close the window
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+        //--------------------------------------------UPDATE-----------------------------------------------
+
+        //--------------------------------------------DRAW-------------------------------------------------
+
+        //clear the window with black color
+
+        window.clear(sf::Color::Black);
+        window.draw(octagon);
+        window.draw(triangle);
+        //draw everything here...
+        //window draw(...);
+        window.display();
+        //--------------------------------------------DRAW-------------------------------------------------
+    }
     return 0;
 }
